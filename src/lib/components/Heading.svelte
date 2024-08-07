f66bebf5be9cb1cdf2a77a3edc2768ce7d761ef8
Ross Wilson <ross.wilson@homeflow.co.uk>
2024-08-07 16:27:44 +0100

    adjust styles of home hero to be responsive

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,3 +1,9 @@
-<h1 class="text-center text-4xl mb-4 tracking-wider">
-  <slot></slot>
+<script>
+  import { twMerge } from 'tailwind-merge';
+
+	export let classes;
+</script>
+
+<h1 class={twMerge('text-center text-4xl mb-4 tracking-wider', classes)}>
+	<slot></slot>
 </h1>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,24 +1,26 @@
 <script>
-  import dogRunning from '$lib/assets/dog-running.jpg';
-  import Heading from '$lib/components/Heading.svelte';
+	import dogRunning from '$lib/assets/dog-running.jpg';
+	import Heading from '$lib/components/Heading.svelte';
 </script>
 
-<section class="relative h-[80vh]">
-  <img
-    src={dogRunning}
-    class="absolute h-full w-full left-0 top-0 cover"
-    alt="Dog running"
-  />
-  <div class="absolute h-full w-full left-0 top-0 cover z-20 bg-black/50"></div>
-  <div class="relative z-50 h-full grid place-items-center">
-    <div class="flex flex-col items-center">
-      <Heading>
-        LKWalks
-      </Heading>
-      <div class="flex gap-4">
-        <button type="button" class="btn variant-filled-primary">Services</button>
-        <button type="button" class="btn variant-filled-tertiary">Prices</button>
-      </div>
-    </div>
-  </div>
+<section class="relative h-[calc(100vh-4rem)]">
+	<img
+		src={dogRunning}
+		class="absolute h-full w-full left-0 top-0 object-cover"
+		alt="Dog running"
+	/>
+	<div class="absolute h-full w-full left-0 top-0 cover z-20 bg-black/30"></div>
+	<div class="relative z-50 h-full grid place-items-center">
+		<div class="h-full w-full flex justify-center flex-col items-center px-4 md:items-start md:px-20 xl:px-48">
+			<Heading classes="text-white">LKWalks</Heading>
+			<p class="max-w-96 mb-4 text-center md:text-left text-white">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam
+			</p>
+			<div class="flex gap-4">
+				<button type="button" class="btn variant-filled-primary">Services</button>
+				<button type="button" class="btn variant-filled-tertiary">Prices</button>
+			</div>
+		</div>
+	</div>
 </section>
